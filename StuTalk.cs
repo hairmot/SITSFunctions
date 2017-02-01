2b15c924b9018fb2ebf11bd902c2aa4e5f8935fb
Colley <pjcc3@leicester.ac.uk>
2017-02-01 10:34:59 +0000

    first com

--- StuTalk.cs
+++ StuTalk.cs
@@ -10,12 +10,11 @@ namespace UoLSitsFunctions
 {
     public class StuTalk
     {
-        static public string Request(StuTalkFunction function, string parameters, string indata, string options = "", string delimiter = "~")
+        static public string Request(StuTalkFunction function, string url, string user, string WSO_PassKeyText, string parameters, string indata, string options = "", string delimiter = "~")
         {     
-            string user = "PJCC3";
-            string passkey = Hash.CalculateMD5Hash(user + "|SITS:VISION|" + "|" + user);
+            string passkey = Hash.CalculateMD5Hash(user + WSO_PassKeyText + "|" + user);
             string md5 = Hash.CalculateMD5Hash(user + passkey + "|SITS|" + delimiter + options + FunctionTranslation[(int)function] + "|VISION|" + parameters + indata);
-            return GetRequest("https://wsv-srsdv.le.ac.uk/uldev/sits.urd/run/siw_wso.http_action?USER=" + user + "&PASSKEY=" + passkey +
+            return GetRequest(url + "USER=" + user + "&PASSKEY=" + passkey +
                      "&DELIMITER=" + delimiter + "&PARAMETERS=" + parameters + "&INDATA=" + indata.Replace("+","%2B") + "&FUNCTION=" + FunctionTranslation[(int)function] + "&MD5=" + md5);
         }
 
